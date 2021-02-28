90858ef98b5476f14a1ad383f27003eb1b6cc3bc
shravan-kaja <shravan.kaja@gmail.com>
2021-02-28 14:48:20 +0530

    Added: method to pop last element

--- src/main/java/com/linkedlist/LinkedList.java
+++ src/main/java/com/linkedlist/LinkedList.java
@@ -22,6 +22,19 @@ public class LinkedList {
         }
     }
 
+    public void popLastElement() {
+        if (head != tail) {
+            INode temp = head;
+            while (temp.getNext() != tail) {
+                temp = temp.getNext();
+            }
+
+            this.tail = temp;
+            INode tempOne = temp.getNext();
+            tempOne = null;
+        }
+    }
+
     public void appendToLinkedList(INode newNode) {
         if (head == null) {
             this.head = newNode;
@@ -42,8 +55,6 @@ public class LinkedList {
 
     public void popFirstElement() {
         this.head = this.head.getNext();
-        //this.head=null;
-
     }
 
     public void printLinkedList() {
@@ -56,6 +67,18 @@ public class LinkedList {
             temp = temp.getNext();
         }
         allNodes.append(temp.getKey());
+        System.out.println(allNodes);
+    }
+
+    public void printAfterPopLast() {
+        StringBuffer allNodes = new StringBuffer();
+        INode temp = head;
+        while (temp.getNext() != null) {
+            allNodes.append(temp.getKey());
+            if (!temp.equals(tail))
+                allNodes.append("->");
+            temp = temp.getNext();
+        }
         System.out.println(allNodes);
     }
 }
