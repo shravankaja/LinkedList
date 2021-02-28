e40ae19c26e319bd23159655868ad4044f3a9048
shravan-kaja <shravan.kaja@gmail.com>
2021-02-28 14:49:09 +0530

    Added: test case to check head and tail positions after deleting

--- src/test/java/com/linkedlist/LinkedListTest.java
+++ src/test/java/com/linkedlist/LinkedListTest.java
@@ -58,6 +58,8 @@ public class LinkedListTest {
         LinkedList linkedList = new LinkedList();
         linkedList.appendToLinkedList(firstNode);
         linkedList.appendToLinkedList(thirdNode);
+        System.out.println("Before Inserting");
+        linkedList.printLinkedList();
         linkedList.insertToLinkedList(firstNode, thirdNode, secondNode);
         System.out.println("Linked list after inserting ");
         linkedList.printLinkedList();
@@ -81,6 +83,24 @@ public class LinkedListTest {
         linkedList.popFirstElement();
         linkedList.printLinkedList();
         boolean result = linkedList.head.equals(secondNode) && linkedList.tail.equals(thirdNode);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void popLastLinkedListTest() {
+        MyNode<Integer> firstNode = new MyNode<>(56);
+        MyNode<Integer> secondNode = new MyNode<>(30);
+        MyNode<Integer> thirdNode = new MyNode<>(70);
+        LinkedList linkedList = new LinkedList();
+        linkedList.appendToLinkedList(firstNode);
+        linkedList.appendToLinkedList(secondNode);
+        linkedList.appendToLinkedList(thirdNode);
+        System.out.println("Linked list before delting :");
+        linkedList.printLinkedList();
+        System.out.println("Linked List after deleteing last element ");
+        linkedList.popLastElement();
+        linkedList.printAfterPopLast();
+        boolean result = linkedList.head.equals(firstNode) && linkedList.tail.equals(secondNode);
         Assertions.assertTrue(result);
     }
 }
