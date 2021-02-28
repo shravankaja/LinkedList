af1b637c8b9a754903da219f0a624b4c89ebccda
shravan-kaja <shravan.kaja@gmail.com>
2021-02-28 12:11:49 +0530

    Added: test case to check if nodes are linked

--- src/test/java/com/linkedlist/LinkedListTest.java
+++ src/test/java/com/linkedlist/LinkedListTest.java
@@ -1,4 +1,17 @@
 package com.linkedlist;
 
+import org.junit.jupiter.api.*;
+
 public class LinkedListTest {
+
+    @Test
+    public void testSimpleLinkedList() {
+        MyNode<Integer> firstNode = new MyNode<>(70);
+        MyNode<Integer> secondNode = new MyNode<>(56);
+        MyNode<Integer> thirdNode = new MyNode<>(30);
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+        boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+        Assertions.assertTrue(result);
+    }
 }
